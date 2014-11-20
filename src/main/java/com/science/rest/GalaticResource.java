f9a2021fc410810838a3f4d9c5a818b41dad215d
sunnee-bridges <matthews.abn@gmail.com>
2014-11-19 20:02:24 -0500

    removed comment code. add todo for logging purposes

--- src/main/java/com/science/rest/GalaticResource.java
+++ src/main/java/com/science/rest/GalaticResource.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Component;
 import com.exceptions.NotFoundException;
 import java.util.HashMap;
 
+
+//TODO add logger to capture more info
 @Component
 @Path("/galatic")
 public class GalaticResource {
@@ -28,32 +30,17 @@ public class GalaticResource {
 	@Produces(MediaType.TEXT_XML)
 	public Response gravityEffect(@PathParam("planet") String planet, @PathParam("weight") Double weight ) {
 
+		double totalMass = 0d;
+		
 		if(!planetGravity.containsKey(planet)) {
-			//Response.status(200).entity(result).build();
-			//throw new NotFoundException("Planet, " + planet + ", is not found");
+			
+			//throw new NotFoundException();
 			throw new NotFoundException("Planet, " + planet + ", is not found");
-			//Response.status(200).entity(result).build()
+
 		}
 		
-		//String result = "<spring>" + planetGravity.get(planet) + "</spring>";
 		
-		double totalMass = 0d;
-    	
-        //System.out.println(planetGravity.size()); 
-        /*
-        HashMap<String, Double> planetGravity = new HashMap<String, Double>();
-        planetGravity.put("earth", 1.0);
-        planetGravity.put("outerspace", 0.0);
-        planetGravity.put("moon", .17);
-        planetGravity.put("venus", .90);
-        planetGravity.put("mars", .38);
-        planetGravity.put("mercury", .38);
-        planetGravity.put("jupiter", 2.36);
-        planetGravity.put("saturn", .92);
-        planetGravity.put("uranus", .89);
-        planetGravity.put("neptune", 1.13);
-        planetGravity.put("pluto", .07); 
-       */
+
         totalMass = weight * planetGravity.get(planet.toLowerCase()); 
        
         String result = "<space><planet>" + planet + "</planet>" + "<weight>" + totalMass + "</weight></space>";
